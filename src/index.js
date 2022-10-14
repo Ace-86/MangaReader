1adb57e9216ff1ff3f9c712304a375dd3a583536
Ace <kronicrespawn@gmail.com>
2022-10-14 13:53:59 -0500

    web scrapper added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("hello")
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+//performing a GET request
+axios.get('https://www.forextradingbig.com/instaforex-broker-review/')
+.then(response => {
+
+  //handling the success
+  const html = response.data;
+
+  //loading response data into a Cheerio instance
+  const $ = cheerio.load(html);
+
+  //selecting the elements with the data
+  const scrapedata = $('a', '.comment-bubble').text();
+
+  //outputting the scraped data
+  console.log(scrapedata);    
+
+})
+.catch( error => {
+  console.log(error);
+}); 
