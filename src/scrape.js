4c7b169738c0363ca6d0bd5c603f4eb3ab446bda
Ace <kronicrespawn@gmail.com>
2022-10-16 13:37:14 -0500

    puppeteer fetch moved to scrape.js

---
+++ src/scrape.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+
+(async () => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    
+    // Specify comic issue page url
+    await page.goto(
+          "https://mangasee123.com/read-online/Weak-Hero-chapter-207.html"
+    );
+    console.log("page has been loaded!");
+
+    const issueSrcs = await page.evaluate(() => {
+      const srcs = Array.from(
+        document.querySelectorAll(".img-fluid")
+      ).map((image) => image.getAttribute("src"));
+      return srcs;
+});
+console.log("Page has been evaluated!");
+
+fs.writeFileSync("./data.json", JSON.stringify(issueSrcs));
+console.log("File is created!");
+
+// End Puppeteer
+await browser.close();
+
+  } catch (err){
+    console.log('you have an error')
+  }
+
+})();
+
