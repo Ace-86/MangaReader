63e210b56eddb2aa77ef123a37de2198f9280597
Ace <kronicrespawn@gmail.com>
2022-10-17 13:52:19 -0500

    class, id added

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const parentNode = document.querySelector('#viewer');
 for (let i = 0; i < data.length; i++) {
     console.log(data[i]);
     const newImage = document.createElement('img'); 
+    newImage.classList.add('manga_page');
+    newImage.setAttribute('id', 'manga_image');
     newImage.src = data[i];
     parentNode.appendChild(newImage);
 }
