f1b1490a55541b7f9e2231fc124442a5c941c507
Ace <kronicrespawn@gmail.com>
2022-10-17 11:39:01 -0500

    loop created to output images

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 const data = require('../data.json');
 console.log(data)
 
+const parentNode = document.querySelector('#viewer');
+
+
+for (let i = 0; i < data.length; i++) {
+    console.log(data[i]);
+    const newImage = document.createElement('img'); 
+    newImage.src = data[i];
+    parentNode.appendChild(newImage);
+}
+
