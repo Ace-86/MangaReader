02991119a869a6681aa150dbb99de08648141aa5
Ace <kronicrespawn@gmail.com>
2022-10-19 13:50:34 -0500

    scripts renamed and moved to seperate folder

---
+++ src/scripts/image_scrape.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+
+(async () => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+   // Specify comic issue page url
+await page.goto(
+  "https://mangasee123.com/read-online/Weak-Hero-chapter-202.html"
+  );
+  console.log("page has been loaded!");
+  const issueSrcs = await page.evaluate(() => {
+    const srcs = Array.from(
+        document.querySelectorAll(".img-fluid")
+      ).map((image) => image.getAttribute("src"));
+    return srcs;
+  });
+  console.log("Page has been evaluated!");
+  const dataCollect = fs.writeFileSync("./src/dataScrape/images.json", JSON.stringify(issueSrcs));
+  console.log("File is created!");
+  console.log(dataCollect)
+// End Puppeteer
+  await browser.close();
+  } catch (err){
+    console.log('you have an error')
+  }
+})();
+
---
+++ src/scripts/list.js
@@ -0,0 +1,18 @@
+const chapterIndex = require('../dataScrape/chapterList.json')
+console.log(chapterIndex)
+const datesIndex = require('../dataScrape/dateList.json')
+console.log(datesIndex);
+
+
+//populates chapters + dates  on page
+const indexList = document.querySelector('#index_list')
+
+for (let i = 0; i < chapterIndex.length; i++) {
+    // console.log(data[i]);
+    const chapterBlock = document.createElement('div'); 
+    chapterBlock.classList.add('chapt_date');
+    chapterBlock.setAttribute('id', 'chapterBlock');
+    chapterBlock.innerText = chapterIndex[i];
+    indexList.appendChild(chapterBlock);
+}
+console.log('hello')
