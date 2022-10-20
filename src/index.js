4dc0a6ec972371fd9e3d0589d91c3d65f5dde8e3
Ace <kronicrespawn@gmail.com>
2022-10-20 11:46:40 -0500

    each page has seperate functionality

--- src/index.js
+++ src/index.js
@@ -1,29 +1,22 @@
-// const data = require('./dataScrape/images.json');
+const data = require('./dataScrape/images.json');
 // console.log(data)
-// const chapterIndex = require('./dataScrape/chapterList.json')
+const chapterIndex = require('./dataScrape/chapterList.json');
 // console.log(chapterIndex)
-// const datesIndex = require('./dataScrape/dateList.json')
+const datesIndex = require('./dataScrape/dateList.json')
 // console.log(datesIndex);
 
-// const parentNode = document.querySelector('#viewer');
-
-// //dont activate unless user selects the manga and chapter they want
-// for (let i = 0; i < data.length; i++) {
-//     // console.log(data[i]);
-//     const newImage = document.createElement('img'); 
-//     newImage.classList.add('manga_page');
-//     newImage.setAttribute('id', 'manga_image');
-//     newImage.src = data[i];
-//     parentNode.appendChild(newImage);
-// }
-
-
-const chapterIndex = require('./dataScrape/chapterList.json');
-console.log(chapterIndex)
-const datesIndex = require('./dataScrape/dateList.json')
-console.log(datesIndex);
-
-
+ if ($("body").data("title") === "page_homepage") {
+    console.log('homepage is working')
+ } else if ($("body").data("title") === "manga_viewer") {
+    const parentNode = document.querySelector('#viewer');
+    for (let i = 0; i < data.length; i++) {
+        const newImage = document.createElement('img'); 
+        newImage.classList.add('manga_page');
+        newImage.setAttribute('id', 'manga_image');
+        newImage.src = data[i];
+        parentNode.appendChild(newImage);
+    }
+} else if ($("body").data("title") === "page_directory") {
 //populates chapters + dates  on page
 const indexList = document.querySelector('#index_list')
 
@@ -38,9 +31,15 @@ for (let i = 0; i < chapterIndex.length; i++) {
     chapterLinks.appendChild(chapterBlock)
 }
 
+//get chapter numbers only to use for pulling up images in viewer
+let chapters = [];
 for (let i = 0; i < chapterIndex.length; i++) {
-chapterIndex[i].replace(/[^\d.-]/g, '');
-console.log(chapter);
+   
+chapters.push(chapterIndex[i].replace(/[^\d.-]/g, ''));
+console.log(chapters);
+} 
+} else {
+    console.log('error')
 }
 /* <a href="/read-online/Weak-Hero-chapter-208.html" class="list-group-item ChapterLink ng-scope" ng-repeat="Chapter in vm.Chapters | limitTo:vm.LimitChapter">
 								<span style="font-weight:600" class="ng-binding">
