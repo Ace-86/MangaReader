ddfbe177aa6b53e2ce3d2b30eb849226f73bd8e4
Ace <kronicrespawn@gmail.com>
2022-10-20 11:48:06 -0500

    object file added

---
+++ src/scripts/objects.js
@@ -0,0 +1,9 @@
+const WeakHero = {
+    title:"Weak Hero",
+    genre:"Action, Drama, School Life, Shounen",
+    type:"Manhwa",
+    released:"2018",
+    status:"Ongoing",
+    description:"Once the bullies target you, it's game over. The school is run by tyrants whose favorite hobby is tormenting the weakest kids in class. But then Gray arrived. This mysterious new student threatens to dismantle the established order. He may be small in stature, but his calculative and brutal fighting leaves unsuspecting opponents lying on the floor begging for mercy. Now the school's toughest bullies have to scramble to take down this new and unusual hero."
+}
+
