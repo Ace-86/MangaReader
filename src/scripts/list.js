c4dd9e72cddde1ad35f76447e87e2b9c1b20c370
Ace <kronicrespawn@gmail.com>
2022-10-22 13:04:22 -0500

    files updated

--- src/scripts/list.js
+++ src/scripts/list.js
@@ -5,13 +5,13 @@ console.log(datesIndex);
 
 
 //populates chapters + dates  on page
-const indexList = document.querySelector('#index_list')
+// const indexList = document.querySelector('#index_list')
 
-for (let i = 0; i < chapterIndex.length; i++) {
-    // console.log(data[i]);
-    const chapterBlock = document.createElement('div'); 
-    chapterBlock.classList.add('chapt_date');
-    chapterBlock.setAttribute('id', 'chapterBlock');
-    chapterBlock.innerText = chapterIndex[i];
-    indexList.appendChild(chapterBlock);
-}
+// for (let i = 0; i < chapterIndex.length; i++) {
+//     // console.log(data[i]);
+//     const chapterBlock = document.createElement('div'); 
+//     chapterBlock.classList.add('chapt_date');
+//     chapterBlock.setAttribute('id', 'chapterBlock');
+//     chapterBlock.innerText = chapterIndex[i];
+//     indexList.appendChild(chapterBlock);
+// }
