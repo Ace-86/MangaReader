bd0e8a9653c46a1c49093fb083c89dafac63db09
Ace <kronicrespawn@gmail.com>
2022-10-22 13:03:37 -0500

    objects added

--- src/scripts/objects.js
+++ src/scripts/objects.js
@@ -4,6 +4,36 @@ const WeakHero = {
     type:"Manhwa",
     released:"2018",
     status:"Ongoing",
-    description:"Once the bullies target you, it's game over. The school is run by tyrants whose favorite hobby is tormenting the weakest kids in class. But then Gray arrived. This mysterious new student threatens to dismantle the established order. He may be small in stature, but his calculative and brutal fighting leaves unsuspecting opponents lying on the floor begging for mercy. Now the school's toughest bullies have to scramble to take down this new and unusual hero."
+    description:"Once the bullies target you, it's game over. The school is run by tyrants whose favorite hobby is tormenting the weakest kids in class. But then Gray arrived. This mysterious new student threatens to dismantle the established order. He may be small in stature, but his calculative and brutal fighting leaves unsuspecting opponents lying on the floor begging for mercy. Now the school's toughest bullies have to scramble to take down this new and unusual hero.",
+    key: "Weak-Hero"
+}
+ 
+const WoodenStick = {
+    title:"+99 Reinforced Wooden Stick",
+    genre:"Action, Drama, Shounen",
+    type:"Manhwa",
+    released:"2022",
+    status:"Ongoing",
+    description: "Welcome to Chrono Life, a virtual reality game where only the strongest survive. Ji-u logs in to Chrono Life, only to be ignored and ridiculed by the other players for his lack of experience, crappy items, and being an overall noob. But when he upgrades his wooden stick to the max reinforcement level of +99, Ji-u realizes he has the power to split continents and slay armies of thousands in just one swing. It’s time to teach the rest of you a lesson, you f*ckers.",
+    key: "99-Reinforced-Wooden-Stick"
 }
 
+const Eleceed = {
+    title:"Eleceed",
+    genre:"Action,Comedy, Supernatural, Shounen",
+    type:"Manhwa",
+    released:"2018",
+    status:"Ongoing",
+    description: "Kaiden - A mysterious ability user who hides inside the body of a street cat. He is later picked up by Jiwoo after being injured after a fight with another ability user. He has a stubborn and bossy personality. Jiwoo - an energetic and talkative high school boy who loves cats. He is very kind and also seem to have a special ability.",
+    key: "Eleceed"
+}
+
+const Arr = ['one', 'two', 'three']
+// export {Eleceed, WoodenStick, WeakHero}
+
+
+
+// key list
+// 99-Reinforced-Wooden-Stick
+// Eleceed
+// Weak-Hero
