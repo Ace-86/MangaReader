5f8ca0043330f136a533bffe6f54b2991e092529
Ace <kronicrespawn@gmail.com>
2022-10-23 13:05:19 -0500

    link js connection test (success)

--- src/scripts/list.js
+++ src/scripts/list.js
@@ -1,17 +1,20 @@
 const chapterIndex = require('../dataScrape/chapterList.json')
-console.log(chapterIndex)
+// console.log(chapterIndex)
 const datesIndex = require('../dataScrape/dateList.json')
-console.log(datesIndex);
+// console.log(datesIndex);
 
+const call = () => {
+// populates chapters + dates  on page
+const indexList = document.querySelector('#manga_list')
 
-//populates chapters + dates  on page
-// const indexList = document.querySelector('#index_list')
+for (let i = 0; i < chapterIndex.length; i++) {
+    // console.log(data[i]);
+    const chapterBlock = document.createElement('div'); 
+    chapterBlock.classList.add('chapt_date');
+    chapterBlock.setAttribute('id', 'chapterBlock');
+    chapterBlock.innerText = chapterIndex[i];
+    indexList.appendChild(chapterBlock);
+}
+}
 
-// for (let i = 0; i < chapterIndex.length; i++) {
-//     // console.log(data[i]);
-//     const chapterBlock = document.createElement('div'); 
-//     chapterBlock.classList.add('chapt_date');
-//     chapterBlock.setAttribute('id', 'chapterBlock');
-//     chapterBlock.innerText = chapterIndex[i];
-//     indexList.appendChild(chapterBlock);
-// }
+export {call}
