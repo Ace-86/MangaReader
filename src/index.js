6ebc6caf2b6c715e4b73e56d47dd085da644b377
Ace <kronicrespawn@gmail.com>
2022-11-04 13:47:01 -0500

    attempt to get buttons to function indivdually

--- src/index.js
+++ src/index.js
@@ -8,10 +8,15 @@ const datesIndex = require('./dataScrape/dateList.json')
 // console.log(datesIndex);
 
 
-
+//Home page
 if ($("body").data("title") === "page_homepage") {
     console.log('homepage is working');
- } else if ($("body").data("title") === "manga_viewer") {
+    let contentSelection = document.querySelectorAll('#select_button');
+    contentSelection.forEach(function() {
+        console.log('hey');
+    })
+//Viewer page;
+} else if ($("body").data("title") === "manga_viewer") {
     const parentNode = document.querySelector('#viewer');
     for (let i = 0; i < data.length; i++) {
         const newImage = document.createElement('img'); 
@@ -20,14 +25,14 @@ if ($("body").data("title") === "page_homepage") {
         newImage.src = data[i];
         parentNode.appendChild(newImage);
     }
-    } else if ($("body").data("title") === "page_directory") {
+//Directory page; 
+} else if ($("body").data("title") === "page_directory") {
       console.log("directory")
       call();
-        
+//Summary page; manga index 
 } else if ($("body").data("title") === "page_manga_index") {
 //populates chapters + dates  on page
 const indexList = document.querySelector('#index_list')
-
 for (let i = 0; i < chapterIndex.length; i++) {
     const chapterLinks = document.createElement('a');
     chapterLinks.href = "#";
@@ -45,6 +50,7 @@ for (let i = 0; i < chapterIndex.length; i++) {
 chapters.push(chapterIndex[i].replace(/[^\d.-]/g, ''));
 console.log(chapters);
 } 
+//if nothing above works
 } else {
     console.log('error')
 }
