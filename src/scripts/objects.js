60b49f143b972656cd46cf111e8567cd49d2d0a2
Ace <kronicrespawn@gmail.com>
2022-11-04 21:14:00 -0500

    dataKey added to object

--- src/scripts/objects.js
+++ src/scripts/objects.js
@@ -5,7 +5,8 @@ const WeakHero = {
     released:"2018",
     status:"Ongoing",
     description:"Once the bullies target you, it's game over. The school is run by tyrants whose favorite hobby is tormenting the weakest kids in class. But then Gray arrived. This mysterious new student threatens to dismantle the established order. He may be small in stature, but his calculative and brutal fighting leaves unsuspecting opponents lying on the floor begging for mercy. Now the school's toughest bullies have to scramble to take down this new and unusual hero.",
-    key: "Weak-Hero"
+    key: "Weak-Hero",
+    dataKey: "$WH"
 }
  
 const WoodenStick = {
@@ -15,7 +16,8 @@ const WoodenStick = {
     released:"2022",
     status:"Ongoing",
     description: "Welcome to Chrono Life, a virtual reality game where only the strongest survive. Ji-u logs in to Chrono Life, only to be ignored and ridiculed by the other players for his lack of experience, crappy items, and being an overall noob. But when he upgrades his wooden stick to the max reinforcement level of +99, Ji-u realizes he has the power to split continents and slay armies of thousands in just one swing. It’s time to teach the rest of you a lesson, you f*ckers.",
-    key: "99-Reinforced-Wooden-Stick"
+    key: "99-Reinforced-Wooden-Stick",
+    dataKey: "$WS"
 }
 
 const Eleceed = {
@@ -25,7 +27,8 @@ const Eleceed = {
     released:"2018",
     status:"Ongoing",
     description: "Kaiden - A mysterious ability user who hides inside the body of a street cat. He is later picked up by Jiwoo after being injured after a fight with another ability user. He has a stubborn and bossy personality. Jiwoo - an energetic and talkative high school boy who loves cats. He is very kind and also seem to have a special ability.",
-    key: "Eleceed"
+    key: "Eleceed",
+    dataKey: "$E"
 }
 
 const Arr = ['one', 'two', 'three']
